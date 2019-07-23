e7e094ed5521a0c01ec694d6bb9eeee96d066320
Shreya241986 <47125080+Shreya241986@users.noreply.github.com>
2019-07-22 19:27:22 -0500

    Update choice.py

--- choice.py
+++ choice.py
@@ -1,4 +1,5 @@
 #from random import choice
+from collections import Counter
 from numpy.random import choice
 possible_destinations = ["Berlin","Hamburg","Munich","Amsterdam","London","Paris","Zurich","Heidelburg","Strasbuorg","Ausgburg","Milan","Rome"]
 #print(choice(possible_destinations))
@@ -16,3 +17,23 @@ print(x2)
 """To avoid repition in choices use replace = False"""
 x3 = choice(possible_destinations, size=(3, 4),replace = False)
 print("\n",x3)
+
+#Weighted random choice
+
+professions = ["scientist", 
+               "philosopher", 
+               "engineer", 
+               "priest", 
+               "programmer"]
+probabilities = [0.2, 0.05, 0.3, 0.15, 0.3]
+
+c = Counter()
+for _ in range(1000):
+    profession = choice(professions, p=probabilities)
+    c[profession] += 1
+    
+print(c)
+s = sum(c.values())
+for el in c:
+    c[el] /= s
+print(c)
